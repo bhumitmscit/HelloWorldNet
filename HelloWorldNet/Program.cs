a53945d82d6e2be28a029881f613f14ea5e99679
Bhumit Patel <bhumitmscit@gmail.com>
2021-12-18 20:48:41 +0530

    Hellow world net

---
+++ HelloWorldNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
